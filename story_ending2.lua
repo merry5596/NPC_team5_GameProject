93da3ee9275988fa29b64e1e5b04dca0c0285c33
yeji <ykim5867@gmail.com>
2021-09-16 19:15:23 +0900

    엔딩 폰트 수정

--- story_ending2.lua
+++ story_ending2.lua
@@ -93,7 +93,7 @@ function scene:create(event)
 	--엔딩 문장--
 	local showEnding = {}
 	endingNum = 0
-	showEnding[1] = display.newText(ending[1], dialogueBox.x, dialogueBox.y, 1000, 0, "fonts/Typo_PapyrusEB.ttf", 40)
+	showEnding[1] = display.newText(ending[1], dialogueBox.x, dialogueBox.y, 1000, 0, "fonts/국립박물관문화재단클래식B.ttf", 27)
 	showEnding[1]:setFillColor(1)
 	showEnding[1].alpha = 0
 
