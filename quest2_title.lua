d71040bec9c1bab7d4d8289e71f2f020af5b025b
parksoya <67586669+parksoya@users.noreply.github.com>
2021-09-17 20:41:53 +0900

    default, 퀘스트 수정 완료

--- quest2_title.lua
+++ quest2_title.lua
@@ -1,22 +1,106 @@
 local composer = require( "composer" )
 local scene = composer.newScene()
 
--- 배경 이미지 흰 바탕으로 대체
-local background = display.newRect(display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
-background:setFillColor(1)
+function scene:create( event )
+local sceneGroup = self.view
+
+-- 임시 배경 --
+local background = display.newImageRect("image/background/오두막내부임시배경.jpg", display.contentWidth, display.contentHeight)
+background.x, background.y = display.contentWidth*0.5, display.contentHeight*0.5
 
 local menuButton = display.newImage("image/component/menu_button.png")
 menuButton.x, menuButton.y = display.contentWidth*0.92, display.contentHeight*0.1
 
--- 퀘스트 박스 이미지 area.png로 대체
-local quest1 = display.newImageRect("image/component/area.png", 700, 200)
+-- 퀘스트 박스--
+local quest1 = display.newImageRect("image/component/퀘스트제시.png", 700, 200)
 quest1.x, quest1.y = display.contentCenterX, display.contentCenterY
 
-local title = display.newText("Quest2 : 숲을 돌아다녀 보자", display.contentCenterX, display.contentCenterY,"fonts/GowunBatang-Bold.ttf", 30)
+--퀘스트 제시--
+local title = display.newText("숲을 돌아다녀 보자", display.contentCenterX+150, display.contentCenterY,"fonts/GowunBatang-Bold.ttf", 30)
 
+local inventoryBox = display.newImage("image/component/inventory_box.png")
+inventoryBox.x, inventoryBox.y = display.contentWidth*0.365, display.contentHeight*0.58
+inventoryBox.isVisible = false
+sceneGroup:insert(inventoryBox)
 
+local scrollbar = display.newImage("image/component/inventory_scroll.png")
+scrollbar.x, scrollbar.y = display.contentWidth*0.6736, display.contentHeight*0.35
+scrollbar.isVisible = false
+sceneGroup:insert(scrollbar)
 
+local function fitImage( displayObject, fitWidth, fitHeight, enlarge )
 
+local scaleFactor = fitHeight / displayObject.height 
+local newWidth = displayObject.width * scaleFactor
+	if newWidth > fitWidth then
+		scaleFactor = fitWidth / displayObject.width 
+	end
+	if not enlarge and scaleFactor > 1 then
+		return
+	end
+	displayObject:scale( scaleFactor, scaleFactor )
+end
+
+local closeButton = display.newImage("image/component/menu_close.png")
+fitImage( closeButton, 50, 50, true )
+closeButton.x, closeButton.y = display.contentWidth*0.6736, display.contentHeight*0.24
+closeButton.isVisible = false
+sceneGroup:insert(closeButton)
+
+--메뉴열기--
+local function menuOpen(event)
+  	if(event.phase == "began") then
+  		composer.setVariable("sceneName", home)
+  		composer.showOverlay("menuScene")
+  	end
+end
+menuButton:addEventListener("touch", menuOpen)
+
+--메뉴 시작화면으로 버튼 클릭시 장면 닫고 타이틀화면으로 이동--
+function scene:closeScene()
+	composer.removeScene("homeScene") --현재 장면 이름 넣기 ex)storyScene
+	composer.gotoScene("scene1")
+end
+end
+
+function scene:show( event )
+local sceneGroup = self.view
+local phase = event.phase
+	
+if phase == "will" then
+	-- Called when the scene is still off screen and is about to move on screen
+elseif phase == "did" then
+	-- Called when the scene is now on screen
+	-- 
+	-- INSERT code here to make the scene come alive
+	-- e.g. start timers, begin animation, play audio, etc.
+end	
+end
+
+function scene:hide( event )
+local sceneGroup = self.view
+local phase = event.phase
+	
+if event.phase == "will" then
+	-- Called when the scene is on screen and is about to move off screen
+	--
+	-- INSERT code here to pause the scene
+	-- e.g. stop timers, stop animation, unload sounds, etc.)
+elseif phase == "did" then
+	-- Called when the scene is now off screen
+	composer.removeScene("home")
+
+end
+end
+
+function scene:destroy( event )
+local sceneGroup = self.view
+	
+-- Called prior to the removal of scene's "view" (sceneGroup)
+-- 
+-- INSERT code here to cleanup the scene
+-- e.g. remove display objects, remove touch listeners, save state, etc.
+end
 ---------------------------------------------------------------------------------
 
 -- Listener setup
--- quest6_text.lua
+++ quest6_text.lua
@@ -1,22 +1,25 @@
 local composer = require( "composer" )
 local scene = composer.newScene()
 
--- 배경 이미지 흰 바탕으로 대체
-local background = display.newRect(display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
-background:setFillColor(1)
+function scene:create( event )
+local sceneGroup = self.view
+
+--배경--
+local background = display.newImageRect("image/background/forest(겨울).png", display.contentWidth, display.contentHeight)
+background.x, background.y = display.contentWidth*0.5, display.contentHeight*0.5
 
 local menuButton = display.newImage("image/component/menu_button.png")
 menuButton.x, menuButton.y = display.contentWidth*0.92, display.contentHeight*0.1
 
--- 퀘스트 박스 이미지 area.png로 대체
-local quest1 = display.newImageRect("image/component/area.png", 850, 300)
+-- 퀘스트 박스--
+local quest1 = display.newImage("image/component/퀘스트클리어.png")
 quest1.x, quest1.y = display.contentCenterX, display.contentCenterY
 
 local options = 
 {
-    text = "선생님의 방에서 약도를 찾았다.\n\n선생님 동료들의 무덤가가 있는 장소인 듯하다.\n\n일단 가보는 것이 좋겠지...\n\n선생님의 말씀을 되새겨보며 가보도록 하자.",
+    text = "선생님의 방에서 약도를 찾았다.\n\n선생님 동료들의 무덤가가 있는 장소인 듯하다. 일단 가보는 것이 좋겠지...\n\n선생님의 말씀을 되새겨보며 가보도록 하자.",
     x = display.contentCenterX,
-    y = display.contentCenterY,
+    y = display.contentCenterY-70,
     font = "fonts/GowunBatang-Bold.ttf",
     fontSize = 20,
     align = "center"
@@ -25,6 +28,92 @@ local options =
 local myText = display.newText( options )
 myText:setFillColor( 1 )
 
+local quest = display.newText("아이템을 터치하여 선생님을 찾기 위한 경험치를 획득하자.", display.contentCenterX, display.contentCenterY+150, "fonts/GowunBatang-Bold.ttf", 20 )
+
+local inventoryBox = display.newImage("image/component/inventory_box.png")
+inventoryBox.x, inventoryBox.y = display.contentWidth*0.365, display.contentHeight*0.58
+inventoryBox.isVisible = false
+sceneGroup:insert(inventoryBox)
+
+local scrollbar = display.newImage("image/component/inventory_scroll.png")
+scrollbar.x, scrollbar.y = display.contentWidth*0.6736, display.contentHeight*0.35
+scrollbar.isVisible = false
+sceneGroup:insert(scrollbar)
+
+local function fitImage( displayObject, fitWidth, fitHeight, enlarge )
+
+local scaleFactor = fitHeight / displayObject.height 
+local newWidth = displayObject.width * scaleFactor
+	if newWidth > fitWidth then
+		scaleFactor = fitWidth / displayObject.width 
+	end
+	if not enlarge and scaleFactor > 1 then
+		return
+	end
+	displayObject:scale( scaleFactor, scaleFactor )
+end
+
+local closeButton = display.newImage("image/component/menu_close.png")
+fitImage( closeButton, 50, 50, true )
+closeButton.x, closeButton.y = display.contentWidth*0.6736, display.contentHeight*0.24
+closeButton.isVisible = false
+sceneGroup:insert(closeButton)
+
+--메뉴열기--
+local function menuOpen(event)
+  	if(event.phase == "began") then
+  		composer.setVariable("sceneName", home)
+  		composer.showOverlay("menuScene")
+  	end
+end
+menuButton:addEventListener("touch", menuOpen)
+
+--메뉴 시작화면으로 버튼 클릭시 장면 닫고 타이틀화면으로 이동--
+function scene:closeScene()
+	composer.removeScene("homeScene") --현재 장면 이름 넣기 ex)storyScene
+	composer.gotoScene("scene1")
+end
+end
+
+function scene:show( event )
+local sceneGroup = self.view
+local phase = event.phase
+	
+if phase == "will" then
+	-- Called when the scene is still off screen and is about to move on screen
+elseif phase == "did" then
+	-- Called when the scene is now on screen
+	-- 
+	-- INSERT code here to make the scene come alive
+	-- e.g. start timers, begin animation, play audio, etc.
+end	
+end
+
+function scene:hide( event )
+local sceneGroup = self.view
+local phase = event.phase
+	
+if event.phase == "will" then
+	-- Called when the scene is on screen and is about to move off screen
+	--
+	-- INSERT code here to pause the scene
+	-- e.g. stop timers, stop animation, unload sounds, etc.)
+elseif phase == "did" then
+	-- Called when the scene is now off screen
+	composer.removeScene("home")
+
+end
+end
+
+function scene:destroy( event )
+local sceneGroup = self.view
+	
+-- Called prior to the removal of scene's "view" (sceneGroup)
+-- 
+-- INSERT code here to cleanup the scene
+-- e.g. remove display objects, remove touch listeners, save state, etc.
+end
+
 ---------------------------------------------------------------------------------
 
 -- Listener setup
@@ -32,5 +121,7 @@ scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
+
+----------------------------------------------------------------------------------
 
 return scene
