c2965bc85d24729a9d9c918480abbccfda56a4e6
parksoya <67586669+parksoya@users.noreply.github.com>
2021-09-18 18:40:43 +0900

    Update default.lua

--- default.lua
+++ default.lua
@@ -12,207 +12,75 @@ background.x, background.y = display.contentWidth*0.5, display.contentHeight*0.5
 local menuButton = display.newImage("image/component/menu_button.png")
 menuButton.x, menuButton.y = display.contentWidth*0.92, display.contentHeight*0.1
 
---레벨--
-local level = display.newImage("image/component/level.png")
-level.x, level.y = display.contentWidth*0.095, display.contentHeight*0.115
-
---레벨에 스코어 표시--
-local score = 0 
-local showScore = display.newText(score, display.contentWidth*0.12, display.contentWidth*0.065, "fonts/GowunBatang-Bold.ttf") 
-showScore:setFillColor(1) 
-showScore.size = 50
-
---호박 보석--
-local yellowg = display.newImage("image/component/보석_노.png")
+--호박--
+local yellowg = display.newImageRect("image/component/보석_노.png", 100,100)
 yellowg.x = display.contentCenterX-500
-yellowg.y = display.contentCenterY+100
+yellowg.y = display.contentCenterY
 
---루비 보석--
-local redg = display.newImage("image/component/보석_빨.png")
+--루비--
+local redg = display.newImageRect("image/component/보석_빨.png", 100, 100)
 redg.x = display.contentCenterX-250
-redg.y = display.contentCenterY
+redg.y = display.contentCenterY-100
 
---에메랄드 보석--
-local greeng = display.newImage("image/component/보석_초.png")
+--에메랄드--
+local greeng = display.newImageRect("image/component/보석_초.png", 100, 100)
 greeng.x = display.contentCenterX
-greeng.y = display.contentCenterY+100
+greeng.y = display.contentCenterY
 
---사파이어 보석--
-local blueg = display.newImage("image/component/보석_파.png")
+--사파이어--
+local blueg = display.newImageRect("image/component/보석_파.png", 100, 100)
 blueg.x = display.contentCenterX+250
-blueg.y = display.contentCenterY
+blueg.y = display.contentCenterY-100
 
---다이아몬드 보석--
-local whiteg = display.newImage("image/component/보석_흰.png")
+--다이아몬드--
+local whiteg = display.newImageRect("image/component/보석_흰.png", 100, 100)
 whiteg.x = display.contentCenterX+500
-whiteg.y = display.contentCenterY+100
-
---보석 설명 박스--
-local box = display.newImageRect("image/component/area.png", 850, 150)
-box.x, box.y = display.contentCenterX, display.contentCenterY
-box.isVisible = false
-
---호박 보석 설명--
-local yellow = display.newText("은은한 색채를 내뿜는 호박. 아름다움이 녹아들어 있습니다. 손톱 보다는 조금 큼지막해요.", display.contentCenterX, display.contentCenterY, "fonts/GowunBatang-Bold.ttf", 20)
-yellow : setFillColor(1)
-yellow.isVisible = false
-
---루비 보석 설명--
-local red = display.newText("곱게 붉은 작은 루비. 손톱보다 작아서 그런 건지, 들고 다니기 아주 간편합니다.", display.contentCenterX, display.contentCenterY,"fonts/GowunBatang-Bold.ttf", 20)
-red : setFillColor(1)
-red.isVisible = false
-
---에메랄드 보석 설명--
-local green = display.newText("맑은 녹빛이 화려게 빛납니다. 꽤 묵직한 걸로 보아, 손가락 하나 정도의 크기인 듯합니다.", display.contentCenterX, display.contentCenterY,"fonts/GowunBatang-Bold.ttf", 20)
-green : setFillColor(1)
-green.isVisible = false
-
---사파이어 보석 설명--
-local blue = display.newText("청명한 여름 하늘을 모아 담은 듯한 보석. 손바닥과 얼추 비슷한 면적입니다.", display.contentCenterX, display.contentCenterY,"fonts/GowunBatang-Bold.ttf", 20)
-blue : setFillColor(1)
-blue.isVisible = false
-
---다이아몬드 보석 설명--
-local white = display.newText("투명하게 반짝이는 다이아몬드. 크기도 큼직해서 그런 걸까요, 탐스럽기 그지 없습니다.", display.contentCenterX, display.contentCenterY,"fonts/GowunBatang-Bold.ttf", 20)
-white : setFillColor(1)
-white.isVisible = false
-
---설명창 닫는 방법 하단 문구--
-local warning = display.newText("설명창 닫으려면 설명창을 클릭하세요.", display.contentCenterX, display.contentCenterY+250, "fonts/GowunBatang-Bold.ttf", 20)
-warning:setFillColor(1)
+whiteg.y = display.contentCenterY
 
 --보석 선택 방법 하단 문구--
-local warning_2 = display.newText("보석을 선택하려면 더블 클릭하세요.", display.contentCenterX, display.contentCenterY+300, "fonts/GowunBatang-Bold.ttf", 20)
+local warning_2 = display.newText("보석을 선택하려면 더블 클릭하세요.", display.contentCenterX-450, display.contentCenterY+320, "fonts/GowunBatang-Bold.ttf", 20)
 warning_2:setFillColor(1)
 
---설명창 오픈--
-local function boxOpen(event)
-	box.isVisible = true
-end
-
-yellowg:addEventListener("mouse", boxOpen)
-redg:addEventListener("mouse", boxOpen)
-greeng:addEventListener("mouse", boxOpen)
-blueg:addEventListener("mouse", boxOpen)
-whiteg:addEventListener("mouse", boxOpen)
-
---호박 보석 설명창 열기--
-local function openyellow(event)
-	yellow.isVisible = true
-	red.isVisible = false
-	green.isVisible = false
-	blue.isVisible = false
-	white.isVisible = false
-end
-
-yellowg:addEventListener("mouse", openyellow)
-
---루비 보석 설명창 열기--
-local function openred(event)
-	red.isVisible = true
-	yellow.isVisible = false
-	green.isVisible = false
-	blue.isVisible = false
-	white.isVisible = false
-end
-
-redg:addEventListener("mouse", openred)
-
---에메랄드 보석 설명창 열기--
-local function opengreen(event)
-	green.isVisible = true
-	red.isVisible = false
-	yellow.isVisible = false
-	blue.isVisible = false
-	white.isVisible = false
-end
-
-greeng:addEventListener("mouse", opengreen)
-
---사파이어 보석 설명창 열기--
-local function openblue(event)
-	blue.isVisible = true
-	yellow.isVisible = false
-	red.isVisible = false
-	green.isVisible = false
-	white.isVisible = false
-end
-
-blueg:addEventListener("mouse", openblue)
-
---다이아몬드 보석 설명창 열기--
-local function openwhite(event)
-	white.isVisible = true
-	yellow.isVisible = false
-	red.isVisible = false
-	green.isVisible = false
-	blue.isVisible = false
-end
-
-whiteg:addEventListener("mouse", openwhite)
-
---설명창 닫기--
-local function boxClose(event)
-	box.isVisible = false 
-	yellow.isVisible = false
-	red.isVisible = false
-	green.isVisible = false
-	blue.isVisible = false
-	white.isVisible = false
-end
-
-box:addEventListener("tap", boxClose)
-yellow:addEventListener("tap", boxClose)
-red:addEventListener("tap", boxClose)
-green:addEventListener("tap", boxClose)
-blue:addEventListener("tap", boxClose)
-white:addEventListener("tap", boxClose)
-
---호박 보석 더블클릭시 레벨 2점 추가--
+--호박 더블클릭시 경험치 2--
 local function yellowtap(event)
 	if ( event.numTaps == 2 ) then
-		score = score + 2
-		showScore.text = score
+		display.newText("호박 선택! 경험치 2를 획득하였습니다.", display.contentCenterX, display.contentCenterY+200, "fonts/GowunBatang-Bold.ttf", 30)
 	end
 end
 
 yellowg:addEventListener("tap", yellowtap)
 
---루비 보석 더블클릭시 레벨 1점 추가--
+--루비 더블클릭시 경험치 1--
 local function redtap(event)
 	if ( event.numTaps == 2) then
-		score = score + 1
-		showScore.text = score
+		display.newText("루비 선택! 경험치 1을 획득하였습니다.", display.contentCenterX, display.contentCenterY+200, "fonts/GowunBatang-Bold.ttf", 30)
 	 end
 end
 
 redg:addEventListener("tap", redtap)
 
---에메랄드 보석 더블클릭시 레벨 3점 추가--
+--에메랄드 더블클릭시 경험치 3--
 local function greentap(event)
 	if ( event.numTaps == 2) then
-		score = score + 3
-		showScore.text = score
+		display.newText("에메랄드 선택! 경험치 3을 획득하였습니다.", display.contentCenterX, display.contentCenterY+200, "fonts/GowunBatang-Bold.ttf", 30)
 	 end
 end
 
 greeng:addEventListener("tap", greentap)
 
---사파이어 보석 더블클릭시 레벨 4점 추가--
+--사파이어 더블클릭시 경험치 4--
 local function bluetap(event)
 	if ( event.numTaps == 2) then
-		score = score + 4
-		showScore.text = score
+		display.newText("사파이어 선택! 경험치 4를 획득하였습니다.", display.contentCenterX, display.contentCenterY+200, "fonts/GowunBatang-Bold.ttf", 30)
 	 end
 end
 
 blueg:addEventListener("tap", bluetap)
 
---다이아몬드 보석 더블클릭시 레벨 5점 추가--
+--다이아몬드 더블클릭시 경험치 5--
 local function whitetap(event)
 	if ( event.numTaps == 2) then
-		score = score + 5
-		showScore.text = score
+		display.newText("다이아몬드 선택! 경험치 5를 획득하였습니다.", display.contentCenterX, display.contentCenterY+200, "fonts/GowunBatang-Bold.ttf", 30)
 	 end
 end
 
@@ -314,3 +182,4 @@ scene:addEventListener( "destroy", scene )
 -----------------------------------------------------------------------------------------
 
 return scene
+
